c4d64074df00ea65f2310a7ebc0ebc8264394ba2
Casper J <casper.jaller@gmail.com>
2021-02-28 21:19:37 +0100

    Update 2021-02-28#21:19

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-let toggleWeather = document.getElementById("weatherbox");
-let toggleAttraction = document.getElementById("attractionbox");
-let toggleFilter = document.getElementById("filterbox");
+const toggleWeather = document.getElementById("weatherbox");
+const toggleAttraction = document.getElementById("attractionbox");
+const toggleFilter = document.getElementById("filterbox");
 let weather, places;
 
 var dateObj = new Date()
@@ -9,6 +9,19 @@ const todayYYYYMMDD = dateObj.toISOString().slice(0,10).replace(/-/g,"");
 
 const btn = document.querySelector('button');
 btn.onclick = getInputValuesAndExecuteSearch;
+
+toggleWeather.onclick = disableAttractions;
+toggleAttraction.onclick = disableWeather;
+
+function disableAttractions(){
+  let disable = document.querySelector('.results');
+  disable.innerHTML = '';
+}
+function disableWeather(){
+  let disable = document.querySelector('.weather');
+  disable.innerHTML = '';
+}
+
 
 async function async_fetch(url) {
   let response = await fetch(url)
@@ -107,59 +120,62 @@ let img = document.createElement('img');
     fragment.appendChild(img);
 
     // append the fragment to the DOM tree
+    
     target.appendChild(fragment);
 
 
       //INJECT PLACES INFO
       let y = await places;
+      
       for (let index = 0; index < y.response.groups[0].items.length; index++) {
-        let path = y.response.groups[0].items[index];
-      
-      let name = path.venue.name;
-      let address = path.venue.location.address;
-      let city = path.venue.location.city;
-      
-      let iconPre = path.venue.categories[0].icon.prefix;
-      let iconSize = '88';
-      let iconSuff = path.venue.categories[0].icon.suffix;
-      let iconPlaceUrl = `${iconPre}${iconSize}${iconSuff}`;
-
-    // compose DOM nodes
-
-    target = document.querySelector('.results');
-    fragment = document.createDocumentFragment();
-
-    article = document.createElement('article');
-        article.textContent = '';
-        article.className = 'attraction';
-        fragment.appendChild(article);
-
-     h1 = document.createElement('h1');
-        h1.textContent = `${name}`;
-        h1.className = 'name';
-        article.appendChild(h1);
-
-     p1 = document.createElement('p');
-        p1.textContent = `${address}`;
-        p1.className = 'address';
-        article.appendChild(p1);
-
-     p2 = document.createElement('p');
-        p2.textContent = `${city}`;
-        p2.className = 'city';
-        article.appendChild(p2);
-
-     img = document.createElement('img');
-        img.src = `${iconPlaceUrl}`;
-        img.className = 'entry-image';
-        img.crossOrigin = "anonymous";
-        article.appendChild(img);
+          let path = y.response.groups[0].items[index];
         
-        target.appendChild(fragment);
+        let name = path.venue.name;
+        let address = path.venue.location.address;
+        let city = path.venue.location.city;
+        
+        let iconPre = path.venue.categories[0].icon.prefix;
+        let iconSize = '88';
+        let iconSuff = path.venue.categories[0].icon.suffix;
+        let iconPlaceUrl = `${iconPre}${iconSize}${iconSuff}`;
+  
+      // compose DOM nodes
+  
+      target = document.querySelector('.results');
+      fragment = document.createDocumentFragment();
+  
+      article = document.createElement('article');
+          article.textContent = '';
+          article.className = 'attraction';
+          fragment.appendChild(article);
+  
+       h1 = document.createElement('h1');
+          h1.textContent = `${name}`;
+          h1.className = 'name';
+          article.appendChild(h1);
+  
+       p1 = document.createElement('p');
+          p1.textContent = `${address}`;
+          p1.className = 'address';
+          article.appendChild(p1);
+  
+       p2 = document.createElement('p');
+          p2.textContent = `${city}`;
+          p2.className = 'city';
+          article.appendChild(p2);
+  
+       img = document.createElement('img');
+          img.src = `${iconPlaceUrl}`;
+          img.className = 'entry-image';
+          img.crossOrigin = "anonymous";
+          article.appendChild(img);
+
+          target.appendChild(fragment);
       }
-      
+
 }
 
+      
 /*
 https://api.foursquare.com/v2/venues/explore?ll=40.7,-74&v=20121124
 GET https://api.foursquare.com/v2/venues/explore
